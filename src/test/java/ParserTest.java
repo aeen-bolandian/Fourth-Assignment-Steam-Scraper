c58192da87b30f47ada982f57b79943dfbbd0ca5
Soroush Saberi <soroush.13830o@gmail.com>
2025-03-30 17:20:56 +0330

    test example fixed

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -20,57 +20,57 @@ public class ParserTest {
     @Test
     public void testSortByName() {
         List<Game> sortedGames = handle.sortByName();
-        Game g1 = new Game("Crusader Kings III" ,4.5,83);
-        Game g2 = new Game("Forza Motorsport 4" ,4.0,80);
-        Game g3 = new Game("Gears of War 3" ,4.2,85);
-        Game g4 = new Game("Killzone 2" ,3.8,62);
-        Game g5 = new Game("Crysis" ,4.6,79);
-        Game g6 = new Game("God of War: Chains of Olympus" ,4.8,88);
+        Game g1 = new Game("Persona 5 Royal" ,4.7,84);
+        Game g2 = new Game("Resident Evil Code: Veronica" ,4.6,85);
+        Game g3 = new Game("The Last of Us Part II" ,4.9,57);
+        Game g4 = new Game("God of War II" ,4.9,90);
+        Game g5 = new Game("Forza Motorsport" ,4.6,76);
+        Game g6 = new Game("Super Mario Sunshine" ,4.5,84);
 
         //Todo
-//        assertEquals(250, sortedCountries.size());
-//        assertEquals(sortedCountries.get(57),g1);
-//        assertEquals(sortedCountries.get(79),g2);
-//        assertEquals(sortedCountries.get(84),g3);
-//        assertEquals(sortedCountries.get(243),g4);
-//        assertEquals(sortedCountries.get(134),g5);
+//        assertEquals(250, sortedGames.size());
+//        assertEquals(sortedGames.get(57),g1);
+//        assertEquals(sortedGames.get(79),g2);
+//        assertEquals(sortedGames.get(84),g3);
+//        assertEquals(sortedGames.get(243),g4);
+//        assertEquals(sortedGames.get(134),g5);
     }
 
         @Test
         public void testSortByPrice() {
             List<Game> sortedGames = handle.sortByPrice();
-            Game g1 = new Game("Crusader Kings III" ,4.5,83);
-            Game g2 = new Game("Forza Motorsport 4" ,4.0,80);
-            Game g3 = new Game("Gears of War 3" ,4.2,85);
-            Game g4 = new Game("Killzone 2" ,3.8,62);
-            Game g5 = new Game("Crysis" ,4.6,79);
-            Game g6 = new Game("God of War: Chains of Olympus" ,4.8,88);
+            Game g1 = new Game("Persona 5 Royal" ,4.7,84);
+            Game g2 = new Game("Resident Evil Code: Veronica" ,4.6,85);
+            Game g3 = new Game("The Last of Us Part II" ,4.9,57);
+            Game g4 = new Game("God of War II" ,4.9,90);
+            Game g5 = new Game("Forza Motorsport" ,4.6,76);
+            Game g6 = new Game("Super Mario Sunshine" ,4.5,84);
 
-//            assertEquals(250, sortedCountries.size());
-//            assertEquals(sortedCountries.get(18),g1);
-//            assertEquals(sortedCountries.get(148),g2);
-//            assertEquals(sortedCountries.get(74),g3);
-//            assertEquals(sortedCountries.get(12),g4);
-//            assertEquals(sortedCountries.get(66),g5);
-//            assertEquals(sortedCountries.get(35),g6);
+//            assertEquals(250, sortedGames.size());
+//            assertEquals(sortedGames.get(18),g1);
+//            assertEquals(sortedGames.get(148),g2);
+//            assertEquals(sortedGames.get(74),g3);
+//            assertEquals(sortedGames.get(12),g4);
+//            assertEquals(sortedGames.get(66),g5);
+//            assertEquals(sortedGames.get(35),g6);
         }
 
     @Test
     public void testSortByRating() {
         List<Game> sortedGames = handle.sortByRating();
-        Game g1 = new Game("Crusader Kings III" ,4.5,83);
-        Game g2 = new Game("Forza Motorsport 4" ,4.0,80);
-        Game g3 = new Game("Gears of War 3" ,4.2,85);
-        Game g4 = new Game("Killzone 2" ,3.8,62);
-        Game g5 = new Game("Crysis" ,4.6,79);
-        Game g6 = new Game("God of War: Chains of Olympus" ,4.8,88);
+        Game g1 = new Game("Persona 5 Royal" ,4.7,84);
+        Game g2 = new Game("Resident Evil Code: Veronica" ,4.6,85);
+        Game g3 = new Game("The Last of Us Part II" ,4.9,57);
+        Game g4 = new Game("God of War II" ,4.9,90);
+        Game g5 = new Game("Forza Motorsport" ,4.6,76);
+        Game g6 = new Game("Super Mario Sunshine" ,4.5,84);
 
-//        assertEquals(250, sortedCountries.size());
-//        assertEquals(sortedCountries.get(11),g1);
-//        assertEquals(sortedCountries.get(166),g2);
-//        assertEquals(sortedCountries.get(97),g3);
-//        assertEquals(sortedCountries.get(67),g4);
-//        assertEquals(sortedCountries.get(24),g5);
-//        assertEquals(sortedCountries.get(2),g6);
+//        assertEquals(250, sortedGames.size());
+//        assertEquals(sortedGames.get(11),g1);
+//        assertEquals(sortedGames.get(166),g2);
+//        assertEquals(sortedGames.get(97),g3);
+//        assertEquals(sortedGames.get(67),g4);
+//        assertEquals(sortedGames.get(24),g5);
+//        assertEquals(sortedGames.get(2),g6);
     }
 }
