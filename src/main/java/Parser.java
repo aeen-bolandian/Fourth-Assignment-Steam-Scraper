c6af44282013ae18e5f1585b8b36ed9971ac6da2
aeen bolandian <bolandian85@gmail.com>
2025-04-21 16:03:18 +0330

    gradle version has been fixded + setUp method bugs have been fixed

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -14,44 +14,44 @@ public class Parser {
     public List<Game>  sortByName(){
 
         games.sort(Comparator.comparing(Game::getName));
-        List<Game> listedGames = Parser.games;
-        return listedGames;
+        return Parser.games;
 
     }
 
     public List<Game> sortByRating(){
 
         games.sort((game1 , game2) -> Double.compare(game2.getRating(), game1.getRating()));
-        List<Game> listedGames = Parser.games;
-        return listedGames;
+        return Parser.games;
 
     }
 
     public List<Game> sortByPrice(){
 
         games.sort((game1 , game2) -> Integer.compare(game2.getPrice(), game1.getPrice()));
-        List<Game> listedGames = Parser.games;
-        return listedGames;
+        return Parser.games;
 
     }
 
     public void setUp() throws IOException {
-
-        File input = new File("D:\\AP\\Fourth-Assignment-Steam-Scraper\\src\\Resources");
-
+        File input = new File("D:\\AP\\Fourth-Assignment-Steam-Scraper\\src\\Resources\\Video_Games.html");
         Document doc = Jsoup.parse(input , "UTF-8");
-
         Elements games = doc.select("div.game");
 
-        for(Element game : games) {
+        for (Element game : games) {
             String name = game.selectFirst(".game-name").text();
-            String price = game.selectFirst(".game-price").text();
-            String rating = game.selectFirst(".game-rating").text();
 
-            Parser.games.add(new Game(name , Double.parseDouble(rating) , Integer.parseInt(price)));
+            String priceText = game.selectFirst(".game-price").text();
+            String priceValue = priceText.replace("€", "").replace(" ", "");
+            int price = Integer.parseInt(priceValue);
+            
+            String ratingText = game.selectFirst(".game-rating").text();
+            String ratingValue = ratingText.split("/")[0].trim();
+            double rating = Double.parseDouble(ratingValue);
 
+            Parser.games.add(new Game(name, rating, price));
         }
     }
+
 
     public static void main(String[] args) {
         //you can test your code here before you run the unit tests
